7c140139f0226549028ca7d908919dae9418f59b
Sebastián Suárez <sebse90@gmail.com>
2021-09-30 11:42:50 -0500

    Continuación HTML funcional
    Adicion de botones nullos e inicio de la tabla HTML y CSS.

--- Ejercicio AJAX/js/misFunciones.js
+++ Ejercicio AJAX/js/misFunciones.js
@@ -3,16 +3,21 @@ function traerInformacion(){
     $.ajax({
         url: "https://ge3e1bb39c96dbd-db202109231828.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/skate/skate",
         type: "GET",
-        datatype: "JSON",
+        datatype: "json",
         cors: false,
         crossDomain: true,
         contentType:'application/json',
         secure: false,
         headers: {
-            'Access-Control-Allow-Origin': false,
+            
+           "Access-Control-Allow-Origin": "*",
+
         },
         success: function(respuesta){
             console.log(respuesta);
+            for(i=0;i<respuesta.items.length;i++){
+                $("#Resultado").append("<br>"+respuesta.items[i].brand+"<br>");
+            }
         }
     })
 
