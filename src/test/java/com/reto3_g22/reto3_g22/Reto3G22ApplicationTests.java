95bc7c35887973e521d4dc3de28c59dfdc19a0e4
unknown <fabian.fhr.99@gmail.com>
2021-10-08 12:17:26 -0500

    archivos del Backend Reto 3

---
+++ src/test/java/com/reto3_g22/reto3_g22/Reto3G22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reto3_g22.reto3_g22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reto3G22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
